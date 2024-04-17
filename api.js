632d12dbe6c1c59b2715ef108d305d122a79c79b
TushShukla267 <129202888+TushShukla267@users.noreply.github.com>
2024-04-17 16:54:03 +0530

    commmenting the api code , will use it later

--- api.js
+++ api.js
@@ -1,14 +1,14 @@
-var city = 'Mumbai';
-$.ajax({
-    method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/weather/' + city, // Corrected URL format
-    headers: { 'X-Api-Key': 'sK85UvTSuRNuxT4piMGbsA=0zlA5GCtc4FUkOiz'}, // Replace [my api key] with your actual API key
-    contentType: 'application/json',
-    success: function(result) {
-        console.log(result); // Log the API response data to the console
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
-});
+// var city = 'Mumbai';
+// $.ajax({
+//     method: 'GET',
+//     url: 'https://api.api-ninjas.com/v1/weather/' + city, // Corrected URL format
+//     headers: { 'X-Api-Key': 'sK85UvTSuRNuxT4piMGbsA=0zlA5GCtc4FUkOiz'}, // Replace [my api key] with your actual API key
+//     contentType: 'application/json',
+//     success: function(result) {
+//         console.log(result); // Log the API response data to the console
+//     },
+//     error: function ajaxError(jqXHR) {
+//         console.error('Error: ', jqXHR.responseText);
+//     }
+// });
 
